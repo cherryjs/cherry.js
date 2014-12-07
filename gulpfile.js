99aefa0c91a7349373ee870b72c62405cfe67370
Jerry Zou <zry656565@gmail.com>
2014-12-07 11:43:27 +0800

    uglify js with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
+'use strict';
+
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task('default', function() {
-    
+    gulp.src('cherry.js')
+        .pipe(uglify())
+        .pipe(rename('cherry.min.js'))
+        .pipe(gulp.dest('.'));
 });
