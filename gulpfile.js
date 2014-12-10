098c46ad0a43cb33a7ffc638718bc5fe1359a1b3
Jerry Zou <zry656565@gmail.com>
2014-12-10 13:31:52 +0800

    add joshing to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,18 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var jshint = require('gulp-jshint');
+
+gulp.task('lint', function() {
+  return gulp.src('cherry.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
 
 gulp.task('default', function() {
     gulp.src('cherry.js')
+    	.pipe(jshint())
+    	.pipe(jshint.reporter('default'))
         .pipe(uglify())
         .pipe(rename('cherry.min.js'))
         .pipe(gulp.dest('.'));
