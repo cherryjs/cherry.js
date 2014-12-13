e905466472451299dba2ed7aade670dc74eeb1df
zlqy386 <zy3861@163.com>
2014-12-13 22:03:05 +0800

    add string test cases

---
+++ test/testcase/String.prototype.js
@@ -0,0 +1,29 @@
+(function($C){
+    QUnit.module('String.prototype', {
+        beforeEach: function() {
+            $C.bind();
+        },
+        afterEach: function() {
+            $C.unbind();
+        }
+    });
+
+    QUnit.test('$trim', function(assert) {
+    	var str1 = "  test  ";
+    	assert.deepEqual(str1.$trim(), "test", 'remove spaces at the start and at the end of the string');
+        var str2 = "   t e s t   ";
+        assert.deepEqual(str2.$trim(), "t e s t", 'won\'t remove spaces in the string');
+        var strEmpty = "";
+        assert.deepEqual(strEmpty.$trim(), "", 'trim an empty string');
+    });
+
+
+    QUnit.test('$removeSpace', function(assert) {
+        var str1 = "  t e s t  ";
+        assert.deepEqual(str1.$removeSpace(), "test", 'remove spaces in the string');
+        var str2 = " t\te\ns\rt ";
+        assert.deepEqual(str2.$removeSpace(), "test", 'remove \\t, \\n, \\r in the string');
+        var strEmpty = "";
+        assert.deepEqual(strEmpty.$trim(), "", 'remove space for an empty string');
+    });
+}(CherryJs));
