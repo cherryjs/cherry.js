1b808d6c97c7f643bcfed4a6cc43c7fb0d1f2776
Jerry Zou <zry656565@gmail.com>
2015-01-08 00:17:10 +0800

    better test cases for Object.prototype

--- test/testcase/Object.prototype.js
+++ test/testcase/Object.prototype.js
@@ -9,10 +9,10 @@
     });
 
     QUnit.test('$clone', function(assert) {
-    	var obj = {attr: "test"};
-    	assert.deepEqual(obj.$clone(), obj, 'can clone an object');
-    	var complexObj = {arr: [1,2,"3"], obj: {attr: "test"}};
-    	assert.deepEqual(complexObj.$clone(), complexObj, 'can clone a complex object');
+        var obj = {attr: "test"};
+        assert.deepEqual(obj.$clone(), obj, 'can clone an object');
+        var complexObj = {arr: [1,2,"3"], obj: {attr: "test"}};
+        assert.deepEqual(complexObj.$clone(), complexObj, 'can clone a complex object');
         var cyclicObj = {};
         var cyclicObj2 = {obj: cyclicObj};
         cyclicObj.obj = cyclicObj2;
@@ -21,15 +21,53 @@
         }, 'return null when cloning a cyclic object.');
     });
 
+    QUnit.test('$isRing', function(assert) {
+        var obj = {
+            a: 1,
+            b: '23',
+            c: {
+                x: { a: 'test' },
+                y: { a: 'test' }
+            }
+        };
+        assert.strictEqual(obj.$isRing(), false, 'judge a object which contains no ring');
+
+        var obj = {},
+            anotherObj = { a: obj };
+        obj.b = anotherObj;
+        assert.strictEqual(obj.$isRing(), true, 'judge a simple object which contains a ring.');
+
+        var obj = { arr: [] },
+            obj1 = {},
+            obj2 = { o: obj1 },
+            obj3 = { o: obj2 };
+        obj1.o = obj3;
+        obj.arr.push(obj2);
+        assert.strictEqual(obj.$isRing(), true, 'judge a complex object which contains a ring.');
+    });
+
     QUnit.test('$equal', function(assert) {
-    	var obj = {attr: "test"};
-    	var obj1 = {attr: "test"};
-    	var obj2 = {att: "test"};
-    	var obj3 = {attr: "Test"};
-    	var obj4 = {att: "test", attr: "Test"};
-    	var complexObj = {arr: [1,2,"3"], obj: {attr: "test"}};
-    	var complexObj1 = {arr: [1,2,"3"], obj: {attr: "test"}};
-    	assert.deepEqual(obj.$equal(obj1), true, 'two objects have the same attribute and value');
-    	assert.deepEqual(complexObj.$equal(complexObj1), true, 'two objects have the same attribute and value');
+        var obj = { 
+            a: "test",
+            b: "test",
+            arr: [1, 2, "3"],
+            subObj: { a: 100 }
+        }, anotherObj = {
+            a: "test",
+            b: "test",
+            arr: [1, 2, "3"],
+            subObj: { a: 100 }
+        };
+        assert.deepEqual(obj.$equal(anotherObj), true, 'two objects have the same attribute and value');
+        
+        //cyclic objects
+        obj = {};
+        anotherObj = { o: obj };
+        obj.o = anotherObj;
+        assert.throws(function() {
+            obj.$equal(anotherObj);
+        }, function(err) {
+            return err.toString() === 'Error 12001: Cannot call $equal on a object which contains a ring.';
+        }, 'throw Error 12001 when calling $equal on cyclicObj');
     });
 }(CherryJs));
